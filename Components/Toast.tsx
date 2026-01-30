7defe06c0dd6373ec2aae9d1be10aad0fb879f41
Somarriba hi <128437648+somarriba02polz@users.noreply.github.com>
2026-01-29 22:02:51 -0600

    Toast Responsive

--- Components/Toast.tsx
+++ Components/Toast.tsx
@@ -16,12 +16,12 @@ export default function Toast() {
     setTimeout(() => {
       toast.classList.add("animate-notificacionSalida");
       toast.addEventListener("animationend", ocultar, { once: true });
-    }, 2000);
+    }, 5000);
   }, []);
   return (
     <div
       ref={toastRef}
-      className={`${toastState ? "bg-green-500" : "bg-red-400"} rounded-md [box-shadow:1px_2px_4px_2px_black] w-3/4 h-18  fixed top-4/5 left-0 right-0 m-auto p-2 flex items-center gap-2 animate-notificacionEntrada`}
+      className={`${toastState ? "bg-green-500" : "bg-red-400"} rounded-md [box-shadow:1px_2px_4px_2px_black] w-3/4 h-18  fixed top-4/5 left-0 right-0 m-auto p-2 flex items-center gap-2 animate-notificacionEntrada lg:w-1/4 lg:m-0 lg:left-auto lg:right-[1.8vw]`}
     >
       <Icon className="size-10" />
       <p className="text-lg font-semibold">
